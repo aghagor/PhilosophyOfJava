33715b2ac900495745b57c4435d86a3d728b0830
aghagor <aghagor@gmail.com>
2017-07-26 12:15:36 +0400

    chapter 3 with tasks

---
+++ src/lessons/chapter2/AutoInc.java
@@ -0,0 +1,17 @@
+package lessons.chapter2;
+
+/**
+ * Created by HCAV on 26.07.2017.
+ */
+public class AutoInc {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("i =: " + i);
+        System.out.println("++i =: " + ++i);
+        System.out.println("i++ =: " + i++);
+        System.out.println("i =: " + i);
+        System.out.println("--i =: " + --i);
+        System.out.println("i-- =: " + i--);
+        System.out.println("i =: " + i);
+    }
+}
