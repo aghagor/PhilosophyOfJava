878677ad5f0d76b11e982d53ba7dd7ddedd73830
aghagor <aghagor@gmail.com>
2017-07-26 10:39:21 +0400

    car speed test task

---
+++ src/tasks/chapter2/CarSpeed.java
@@ -0,0 +1,35 @@
+package tasks.chapter2;
+
+/**
+ * Created by HCAV on 25.07.2017.
+ */
+public class CarSpeed {
+    int speed;
+    int distance;
+    int time;
+
+    public static int speed(int distance, int time) {
+        int speed = distance / time;
+        return speed;
+    }
+
+    public static int distance(int time, int speed) {
+        int distance = time * speed;
+        return distance;
+    }
+
+    public static int time(int speed, int distance) {
+        int time = distance / speed;
+        return time;
+    }
+}
+
+class CarSpeedTest {
+    public static void main(String[] args) {
+        CarSpeed carSpeed = new CarSpeed();
+
+        System.out.println("Car speed is: " + carSpeed.speed(120, 1) + " km/h");
+        System.out.println("The distance: " + carSpeed.distance(2, 60));
+        System.out.println("The time: " + carSpeed.time(50,100));
+    }
+}
