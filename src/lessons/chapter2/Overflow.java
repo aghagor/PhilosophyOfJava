ac3eb4f3cc2ea71c1e606ab485714701c9ff6d29
aghagor@gmail.com <aghagor@gmail.com>
2017-07-30 23:41:20 +0400

    finishing chapter 3git add .!

---
+++ src/lessons/chapter2/Overflow.java
@@ -0,0 +1,13 @@
+package lessons.chapter2;
+
+/**
+ * Created by Goro on 30.07.2017.
+ */
+public class Overflow {
+    public static void main(String[] args) {
+        int big = Integer.MAX_VALUE;
+        System.out.println("Maximum: " + big);
+        int bigger = big * 4;
+        System.out.println("bigger: " + bigger);
+    }
+}
