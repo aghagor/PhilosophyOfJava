284e5eb342d32fbc38b06e6deb77aea9e3da8cad
aghagor@gmail.com <aghagor@gmail.com>
2017-07-30 20:05:17 +0400

    continuing chapter 3

--- src/tasks/chapter1/ATypename.java
+++ src/tasks/chapter1/ATypename.java
@@ -3,7 +3,7 @@ package tasks.chapter1;
 /**
  * Created by HCAV on 25.07.2017.
  */
-public class ATypeName {
+ class ATypeName {
     public static void main(String[] args) {
         ATypeName name = new ATypeName();
         System.out.println(name.getClass());
