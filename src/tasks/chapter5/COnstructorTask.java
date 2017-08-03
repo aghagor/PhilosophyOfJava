e7a3e9aefebca71e5106dca0d13691bc74bd7dbe
aghagor@gmail.com <aghagor@gmail.com>
2017-08-03 23:12:59 +0400

    ...chapter5...

--- src/tasks/chapter5/COnstructorTask.java
+++ src/tasks/chapter5/COnstructorTask.java
@@ -4,4 +4,17 @@ package tasks.chapter5;
  * Created by Goro on 03.08.2017.
  */
 public class COnstructorTask {
+    public COnstructorTask() {
+        System.out.println("This is empty constructor");
+    }
+    COnstructorTask(String s){
+        System.out.println("You pass: "+s);
+    }
+}class ConstructorTaskTest{
+    public static void main(String[] args) {
+        COnstructorTask constructorTask = new COnstructorTask();
+        System.out.println(constructorTask);
+        COnstructorTask ct = new COnstructorTask("Hello Java");
+        System.out.println(ct);
+    }
 }
--- src/tasks/chapter5/Dog.java
+++ src/tasks/chapter5/Dog.java
@@ -4,4 +4,43 @@ package tasks.chapter5;
  * Created by Goro on 03.08.2017.
  */
 public class Dog {
+    void bark(char x) {
+        System.out.println("Gaf gaf");
+    }
+
+    void bark(byte b) {
+        System.out.println("haf haf");
+    }
+
+    void bark(short s) {
+        System.out.println("gaaaf gaaaaaaaf");
+    }
+
+    void bark(int i) {
+        System.out.println("baf baf");
+    }
+
+    void bark(long l) {
+        System.out.println("brrr brrrr");
+    }
+
+    void bark(float f,int i) {
+        System.out.println("gaf gaf gaf");
+    }
+
+    void bark(int i,double d) {
+        System.out.println("haf haf haf");
+    }
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+        d.bark(1);
+        d.bark('x');
+        d.bark(4);
+        d.bark(3);
+        d.bark(15478451541543145l);
+        d.bark(2.3f,2);
+        d.bark(5,3.1);
+    }
 }
