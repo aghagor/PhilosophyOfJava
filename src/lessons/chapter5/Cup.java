a43aa82ba6abeea72c4649437c67bf1a0438fcc0
aghagor@gmail.com <aghagor@gmail.com>
2017-08-09 17:23:14 +0400

    chapter 5... static initializations

---
+++ src/lessons/chapter5/Cup.java
@@ -0,0 +1,39 @@
+package lessons.chapter5;
+
+import net.mindview.util.Print;
+
+import static net.mindview.util.Print.*;
+
+/**
+ * Created by Goro on 09.08.2017.
+ */
+public class Cup {
+    Cup(int marker) {
+        print("Cup(" + marker + ")");
+    }
+
+    void f(int marker) {
+        print("f(" + marker + ")");
+    }
+}
+
+class Cups {
+    static Cup cup1;
+    static Cup cup2;
+
+    static {
+        cup1 = new Cup(1);
+        cup2 = new Cup(2);
+    }
+
+    Cups() {
+        print("Cups()");
+    }
+}
+
+class ExplicitStatic {
+    public static void main(String[] args) {
+        print("Inside main()");
+        Cups.cup1.f(99);
+    }
+}
